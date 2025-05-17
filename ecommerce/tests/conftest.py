36738f24a665b6a19524cc40437b5d67dfbe0f99
Ashish Mishra <ashish.mishra36@gmail.com>
2025-05-17 13:23:26 -0400

    updated dockerfile

--- ecommerce/tests/conftest.py
+++ ecommerce/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
 # from selenium.webdriver.firefox.options import Options
 from utils.logger import generate_logger
 
@@ -9,7 +10,7 @@ from utils.logger import generate_logger
 #     return generate_logger('test_logs')
 
 
-@pytest.fixture(params=['chrome'], scope='class')
+@pytest.fixture(params=['docker'], scope='class')
 def init_driver(request):
     print(f'Test name is : {request.node.name}')
     web_driver= None
@@ -21,6 +22,14 @@ def init_driver(request):
         chrome_options.add_argument("--disable-dev-shm-usage")
         web_driver = webdriver.Chrome(options=chrome_options)
         # web_driver = webdriver.Chrome()
+    if request.param== 'docker':
+        print('----------------------------Setting up----------------------------')
+        options = ChromeOptions()
+        options.browser_name = 'chromium'  # Force Chromium
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        web_driver = webdriver.Remote(options=options)
     # if request.param=='firefox':
     #     # firefox_options = Options()
     #     # firefox_options.add_argument("--headless")
